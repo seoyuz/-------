494319b90f96b0dd67a0bac3e8654963d5118a3a
seoyuz <138371500+seoyuz@users.noreply.github.com>
2024-06-19 17:57:42 +0900

    Docs: 반응형 작업

--- js/common.js
+++ js/common.js
@@ -1,35 +1,41 @@
 $(function() {
-    $(".menu-mobile-btn").on("click", function() {
-        $(this).toggleClass("close")
-        $(".header-mobile-wrap").toggleClass("open")
+    //let $win = $(window);
+    //let $winWidth = $win.innerWidth;
+    let $menuMobileBtn = $(".menu-mobile-btn");
+    let $headerMobileWrap = $(".header-mobile-wrap");
 
-        /* if($(this).hasClass("open")) {
-            $(".header-mobile").removeClass("open")
-        } else {
-            $(".header-mobile").addClass("open")
-        } */
+    //윈도우 사이즈가 변경될 경우
+    $(window).on("resize", function() {
+        mobileHeaderEvent();
     });
 
-    $(".depth1 a img").on("click", function() {
+    //윈도우 사이즈 1024 이상되면 모바일메뉴 닫힘 //
+    function mobileHeaderEvent() {
+        if($(window).innerWidth() > 1024) {
+            $headerMobileWrap.removeClass("open");
+            $menuMobileBtn.removeClass("close");
+        }
+    }
 
-        //$(this).attr("src", "../images/ico_more_open.png")
-        //$(this).parents().siblings(".depth2").slideToggle();
+    // 모바일용 햄버거메뉴 작동 //
+    $menuMobileBtn.on("click", function() {
+        $(this).toggleClass("close")
+        $headerMobileWrap.toggleClass("open")
+    });
+
+    // depth1 더보기 버튼//
+    $(".depth1 a img").on("click", function() {
 
         if(!$(this).parent().hasClass("active")){
             $(this).attr("alt", "닫기");
             $(this).attr("src", "../images/ico_more_open.png");
             $(this).parent().addClass("active");
             $(this).parents().siblings(".depth2").slideDown();
-            /* $(this).attr("src", "../images/ico_more.png") */
         }else{
             $(this).attr("src", "../images/ico_more.png");
             $(this).attr("alt", "열기");
             $(this).parent().removeClass("active");
             $(this).parents().siblings(".depth2").slideUp();
-            /* $(this).attr("src", "../images/ico_more_open.png") */
         }
-
-        /*$(this).parents().siblings(".depth2").slideUp();*/
-        /*$(this).attr("src", "../images/ico_more.png")*/
     });
 });
